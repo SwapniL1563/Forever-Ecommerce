0e54f212cfc1b161b63c43b5dad532e7741b900a
SwapniL1563 <choratswapnil15@gmail.com>
2024-12-06 22:19:18 +0530

    final changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{port:5173},
+  build: {
+    rollupOptions: {
+      external: [
+        'react-router-dom',
+        'react-toastify',
+        'react-toastify/dist/ReactToastify.css',
+        'axios'
+      ]
+    }
+  }
 })
